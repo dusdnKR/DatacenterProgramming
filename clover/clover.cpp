6871024a5ad470f082fbc18a53dfef95477f046d
dusdnKR <84877632+dusdnKR@users.noreply.github.com>
2022-04-26 16:17:28 +0900

    Add files via upload

---
+++ clover/clover.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+
+	cout << "<³×ÀÙ Å¬·Î¹ö>" << endl << endl;
+
+	Sleep(1000);
+	cout << "±í°í ÀÛÀº" << endl;
+
+	Sleep(1000);
+	cout << "»ê°ñÂ¥±â" << endl;
+
+	Sleep(1000);
+	cout << "»ç" << endl;
+
+	Sleep(1000);
+	cout << "ÀÌ·Î" << endl;
+
+	Sleep(1000);
+	cout << "¸¼Àº ¹°" << endl;
+
+	Sleep(1000);
+	cout << "Èå¸£´Â" << endl;
+
+	Sleep(1000);
+	cout << "ÀÛÀº »ùÅÍ¿¡" << endl;
+
+	Sleep(2000);
+	cout << "¿¹»Û ²Éµé" << endl;
+
+	Sleep(1000);
+	cout << "»çÀÌ¿¡" << endl;
+
+	Sleep(1000);
+	cout << "»ìÂ¦ ¼û°ÜÁø" << endl;
+
+	Sleep(2000);
+	cout << "ÀÌ½½ ¸Ô°í" << endl;
+
+	Sleep(1000);
+	cout << "ÇÇ¾î³­" << endl;
+
+	Sleep(1000);
+	cout << "³×ÀÙ Å¬·Î¹ö" << endl;
+
+	Sleep(1500);
+	cout << "¶ö¶ö¶ó" << endl;
+
+	Sleep(500);
+	cout << "ÇÑ" << endl;
+
+	Sleep(1000);
+	cout << "ÀÙ" << endl;
+
+	Sleep(500);
+	cout << "¶ö¶ö¶ó" << endl;
+
+	Sleep(500);
+	cout << "µÎ" << endl;
+
+	Sleep(1000);
+	cout << "ÀÙ" << endl;
+
+	Sleep(500);
+	cout << "¶ö¶ö¶ó" << endl;
+
+	Sleep(500);
+	cout << "¼¼" << endl;
+
+	Sleep(1000);
+	cout << "ÀÙ" << endl;
+
+	Sleep(500);
+	cout << "¶ö¶ö¶ó" << endl;
+
+	Sleep(500);
+	cout << "³×" << endl;
+
+	Sleep(500);
+	cout << "~" << endl;
+
+	Sleep(500);
+	cout << "ÀÙ" << endl;
+
+	Sleep(1000);
+	cout << "Çà¿îÀ»" << endl;
+
+	Sleep(1500);
+	cout << "°¡Á®´Ù" << endl;
+
+	Sleep(500);
+	cout << "ÁØ´Ù´Â" << endl;
+
+	Sleep(2000);
+	cout << "¼öÁÝÀº" << endl;
+
+	Sleep(1000);
+	cout << "¾ó±¼ÀÇ ¹Ì¼Ò" << endl;
+
+	Sleep(3000);
+	cout << "ÇÑÁÙ±âÀÇ" << endl;
+
+	Sleep(1000);
+	cout << "µû½ºÇÑ" << endl;
+
+	Sleep(1000);
+	cout << "ÇÞ»ì ¹ÞÀ¸¸ç" << endl;
+
+	Sleep(2000);
+	cout << "Èñ¸ÁÀ¸·Î" << endl;
+
+	Sleep(1000);
+	cout << "°¡µæÇÑ" << endl;
+
+	Sleep(1000);
+	cout << "³ªÀÇ Ä£±¸¾ß" << endl;
+
+	Sleep(1500);
+	cout << "ºûÃ³·³" << endl;
+
+	Sleep(1500);
+	cout << "¹à" << endl;
+
+	Sleep(500);
+	cout << "~Àº" << endl;
+
+	Sleep(500);
+	cout << "¸¶À½À¸·Î" << endl;
+
+	Sleep(2000);
+	cout << "³Ê¸¦" << endl;
+
+	Sleep(1000);
+	cout << "´à°í ½Í¾î" << endl;
+
+	Sleep(5000);
+
+	return 0;
+}
+
+// ¼ÒÇÁÆ®¿þ¾îÀ¶ÇÕÇÐ°ú 2021105685 ±è¿¬¿ì
